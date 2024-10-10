686d4e1636545f806157f6c2a758f023c5990438
Anton <98217668+antuann8@users.noreply.github.com>
2024-10-10 17:35:19 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import { useState } from 'react'
-<<<<<<< HEAD
-import { ChipsInput } from './components/ChipsInput'
-=======
 import { ChipsInput } from './components/ChipsInput/ChipsInput'
->>>>>>> 299e5ab (update)
 
 function App() {
 	const [value, setValue] = useState('')
